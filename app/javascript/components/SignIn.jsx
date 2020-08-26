9433d878577657ec6dea6e8a09860deebe783862
Ranjeet Jagtap <ranjeet.jagtap43@gmail.com>
2020-08-26 11:53:48 +0530

    got rid of unnecessary code from sign in component

--- app/javascript/components/SignIn.jsx
+++ app/javascript/components/SignIn.jsx
@@ -2,8 +2,6 @@ import React, { useState, useContext } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
@@ -73,10 +71,6 @@ export default function SignIn(props) {
               value={password}
               onChange={e=> setPassword(e.target.value)}
             />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
             <Button
               type="submit"
               fullWidth
@@ -86,18 +80,6 @@ export default function SignIn(props) {
             >
               Sign In
             </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href='/sign_up' variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
           </form>
         </div>
       </Grid>
